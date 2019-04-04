e6b25c2e74157140a22fdebe61bc53ae1b6d448a
idRit <mritwik369@gmail.com>
2019-04-04 11:36:47 +0530

    all staged

---
+++ api.py
@@ -0,0 +1,49 @@
+from database-connector import databaseModel.dbModel
+from flask import Flask, url_for, request, json, Response, jsonify
+from flask_cors import CORS, cross_origin
+
+interactor = dbModel()
+interactor.connected()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods = ['GET'])
+def index():
+    return jsonify({"Connected" : "Connected"})
+
+@app.route("/api/insertEndUser", methods = ['POST'])
+def insertEndUser():
+    res = json.dumps(request.json)
+    resDict = json.loads(res)
+
+    aadhaar = resDict.get("aadhaar")
+    password = resDict.get("password")
+
+    error = interactor.insert_one(aadhaar, password)
+
+    if error is None:
+        return jsonify({"notify" : "no error"})
+    else 
+        return jsonify({"notify" : "error"})
+
+@app.route("/api/checkUser", methods = ['POST'])
+def checkUser():
+    res = json.dumps(request.json)
+    resDict = json.loads(res)
+
+    aadhaar = resDict.get("aadhaar")
+    password = resDict.get("password")
+
+    error = interactor.get_one_detail(aadhaar, password)
+
+    if error == 1:
+        return jsonify({"notify" : "1"})
+    elif error == 0:
+        return jsonify({"notify" : "0"})
+    elif error == 3:
+        return jsonify({"notify" : "3"})
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ database-connector/databaseModel.py
@@ -0,0 +1,46 @@
+from pymongo import MongoClient
+import bcrypt
+
+class dbModel:
+    
+    def __init__(self):
+        self.client = MongoClient("mongodb+srv://red:blxgre369@cluster0-oaiys.mongodb.net/test?retryWrites=true")
+        self.cred_db = self.client.credentials
+        self.endUserCollection = self.cred_db.endUser
+
+    def insert_one(self, aadhaar, pwd): 
+        if aadhaar is None:
+            return "Enter aadhar"
+
+        if pwd is None:
+            return "Enter pwd"
+
+        hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())    
+
+        end_user_details = {
+            "aadhaar" : aadhaar,
+            "password" : hashed
+        }
+
+        self.endUserCollection.insert(end_user_details)
+
+    def get_one_detail(self, aadhar, pwd):  
+        if aadhaar is None:
+            return 3 
+        
+        if pwd is None:
+            return 2 
+
+        cred = self.endUserCollection.find_one({"aadhaar" : aadhar})
+
+        if cred:
+            if bcrypt.checkpw(cred.get("password"), pwd) :
+                return 1 #correct pwd
+
+            else:
+                return 0 #faltu pwd
+        else :
+            return 3 #user not found
+
+        def connected(self):
+            print('connected')
