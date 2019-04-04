fa40b0fa39b5ea036a43901943c6e344e3da2224
idRit <mritwik369@gmail.com>
2019-04-04 18:07:02 +0530

    fb

--- databaseconnector/databaseModel.py
+++ databaseconnector/databaseModel.py
@@ -11,68 +11,68 @@ class dbModel:
         self.aadhaar_data = self.client.aadaar_data
         self.data = self.aadhaar_data.a_data
 
-    def insert_one(self, aadhaar, pwd): 
-        if aadhaar is None:
-            return 3
-
-        if pwd is None:
-            return 0
-
-        cred = self.endUserCollection.find_one({"aadhaar" : aadhaar})
-
-        if cred is not None:
-            if bcrypt.checkpw(pwd.encode(), cred.get("password")) :
-                return 1 #correct pwd
-
-            else:
-                return 0 #faltu pwd
-
-        else :
-            self.get_one_detail(aadhaar, pwd)
-
-    def get_one_detail(self, aadhar, pwd):  
-        if aadhar is None:
-            return 3 
-        
-        if pwd is None:
-            return 2 
-
-        cred = self.data.find_one({"aadaar_no" : aadhar})
-
-        if cred is not None: 
-            hashed = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())    
-
-            end_user_details = {
-                "aadhaar" : aadhar,
-                "password" : hashed
-            }
-            
-            self.endUserCollection.insert(end_user_details)
-            return 1
-        else :
-            return 3 #user not found
-
-    def hybrid_insert(self, ad, pwd):
-        cred = self.endUserCollection.find_one({"aadhaar" : ad})
-        if cred is None:
-            cred = self.data.find_one({"aadaar_no" : ad})
-            if cred is not None: 
-                hashed = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())    
-
-                end_user_details = {
-                    "aadhaar" : ad,
-                    "password" : hashed
-                }
-            
-                self.endUserCollection.insert(end_user_details)
-                return 0#user added
-            else:
-                return 3#adhar not exist
-        else: 
-            if bcrypt.checkpw(pwd.encode(), cred.get("password")) :
-                return 1 #correct pwd
-            else:
-                return 2#faltu pwd    
+    #def insert_one(self, aadhaar, pwd): 
+    #    if aadhaar is None:
+    #        return 3
+#
+    #    if pwd is None:
+    #        return 0
+#
+    #    cred = self.endUserCollection.find_one({"aadhaar" : aadhaar})
+#
+    #    if cred is not None:
+    #        if bcrypt.checkpw(pwd.encode(), cred.get("password")) :
+    #            return 1 #correct pwd
+#
+    #        else:
+    #            return 0 #faltu pwd
+#
+    #    else :
+    #        self.get_one_detail(aadhaar, pwd)
+#
+    #def get_one_detail(self, aadhar, pwd):  
+    #    if aadhar is None:
+    #        return 3 
+    #    
+    #    if pwd is None:
+    #        return 2 
+#
+    #    cred = self.data.find_one({"aadaar_no" : aadhar})
+#
+    #    if cred is not None: 
+    #        hashed = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())    
+#
+    #        end_user_details = {
+    #            "aadhaar" : aadhar,
+    #            "password" : hashed
+    #        }
+    #        
+    #        self.endUserCollection.insert(end_user_details)
+    #        return 1
+    #    else :
+    #        return 3 #user not found
+#
+    #def hybrid_insert(self, ad, pwd):
+    #    cred = self.endUserCollection.find_one({"aadhaar" : ad})
+    #    if cred is None:
+    #        cred = self.data.find_one({"aadaar_no" : ad})
+    #        if cred is not None: 
+    #            hashed = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())    
+#
+    #            end_user_details = {
+    #                "aadhaar" : ad,
+    #                "password" : hashed
+    #            }
+    #        
+    #            self.endUserCollection.insert(end_user_details)
+    #            return 0#user added
+    #        else:
+    #            return 3#adhar not exist
+    #    else: 
+    #        if bcrypt.checkpw(pwd.encode(), cred.get("password")) :
+    #            return 1 #correct pwd
+    #        else:
+    #            return 2#faltu pwd    
 
 
     def insertOne(self, ad, pwd):
